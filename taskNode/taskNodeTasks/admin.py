3cb361075c29c60ca8c5697b31a9ae937767033c
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-10-25 00:57:54 +0300

    [ADD] Admin files

--- taskNode/taskNodeTasks/admin.py
+++ taskNode/taskNodeTasks/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Task, Category, UserTask, Comment, Notification
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'priority', 'created_by')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_by', 'updated_at')
+
+
+@admin.register(UserTask)
+class UserTaskAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'task_id', 'role', 'assigned_at')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'task_id', 'created_at')
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'task_id', 'created_at')
