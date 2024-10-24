ed8aaedf22667c8d01396f7c53da8df3fea68848
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-10-25 01:04:37 +0300

    [MOD] Tasks serializers file

--- taskNode/taskNodeTasks/serializes.py
+++ taskNode/taskNodeTasks/serializes.py
@@ -1,23 +1,41 @@
 from rest_framework import serializers
 from .models import *
+from taskNodeUser.models import CustomUser
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class UserTaskSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
+
     class Meta:
-        model = Category
-        fields = '__all__'
+        model = UserTask
+        fields = ['user', 'role']
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    users = UserTaskSerializer(many=True, write_only=True)
+
     class Meta:
         model = Task
         fields = '__all__'
 
+    def create(self, validated_data):
+        users_data = validated_data.pop('users')
+        task = Task.objects.create(**validated_data)
 
-class UserTaskSerializer(serializers.ModelSerializer):
+        for user_data in users_data:
+            UserTask.objects.create(
+                user=user_data['user'],
+                task=task,
+                role=user_data['role'],
+            )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    related_category = TaskSerializer(read_only=True, many=True, source='related_category')
+
     class Meta:
-        model = UserTask
-        fields = '__all__'
+        model = Category
+        exclude = ['category_id']
 
 
 class NotificationSerializer(serializers.ModelSerializer):
@@ -30,3 +48,4 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        
