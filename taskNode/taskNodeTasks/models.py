039a3ac5957ab7c9b3401b1015714f906c513ddf
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-10-25 01:00:39 +0300

    [ADD] Task models

--- taskNode/taskNodeTasks/models.py
+++ taskNode/taskNodeTasks/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     category_id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=2000)
-    created_at = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='Related User')
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='related_user')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
 
@@ -26,6 +26,7 @@ STATUS = [
     ('COMPLETED', 'Completed')
 ]
 
+
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
@@ -33,13 +34,18 @@ class Task(models.Model):
     status = models.CharField(choices=STATUS, max_length=255)
     priority = models.CharField(choices=PRIORITIES, max_length=255)
     deadline = models.DateTimeField()
-    project_id = models.ForeignKey(Project.project_id, on_delete=models.CASCADE, related_name='Related Project', null=True)
-    category_id = models.ForeignKey(Category.category_id, on_delete=models.CASCADE, related_name='Related Category', null=True)
-    team_id = models.ForeignKey(Team.team_id, on_delete=models.CASCADE, related_name='Related Team', null=True)
-    created_by = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='user')
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='related_project', null=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='related_category', null=True)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='related_team', null=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
 
+    def __str__(self):
+        return f'{self.title}, {self.status}'
+    
+    class Meta:
+        ordering = ('title', 'status')
 
 ROLES = [
     ('ASSIGNEE', 'Assignee'),
@@ -50,24 +56,24 @@ ROLES = [
 
 class UserTask(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE)
-    task_id = models.ForeignKey(Task.task_id, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     role = models.CharField(choices=ROLES, max_length=255)
     assigned_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
-    task_id = models.ForeignKey(Task.task_id, on_delete=models.CASCADE, related_name='Related Task')
-    user_id = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='Related User')
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='task_comment')
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_comment')
     content = models.TextField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Notification(models.Model):
     notification_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='Related User')
-    task_id = models.ForeignKey(Task.task_id, on_delete=models.CASCADE, related_name='Related Task')
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_notification')
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='task_notification')
     message = models.TextField(max_length=2000)
     read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)   
