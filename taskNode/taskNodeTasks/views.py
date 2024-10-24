200afe306d15e5ab2e63c28db1619a444a373f08
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-10-25 01:05:53 +0300

    [MOD] Task Views file

--- taskNode/taskNodeTasks/views.py
+++ taskNode/taskNodeTasks/views.py
@@ -1,119 +1,133 @@
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.http import Http404
 from .serializes import *
 from .models import *
 
 
 #Task Model CRUD operations
-
-@api_view(['GET'])
-def get_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-def create_new_task(request):
-    serializer = TaskSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['PUT'])
-def update_task(request, task_id):
-    try:
-        task = Task.objects.all(task_id=task_id)
-    except Task.DoesNotExist:
-        return Response({'error:': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = TaskSerializer(task, data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
+class TaskListView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['DELETE'])
-def delete_task(request, task_id):
-    try:
-        task = Task.objects.all(task_id=task_id)
-    except Task.DoesNotExist:
-        return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
-    
-    task.delete()
-    return Response({'message': 'task deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+class TaskDetailView(APIView):
+    def get_object(self, task_id):
+        try:
+            return Task.objects.get(task_id=task_id)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, task_id):
+        task = self.get_object(task_id)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, task_id):
+        task = self.get_object(task_id=task_id)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, task_id):
+        task = self.get_object(task_id)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 #Category Model CRUD operations
 
-@api_view(['GET'])
-def get_categories(request):
-    category = Category.objects.all()
-    serializer = CategorySerializer(category, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def create_category(request):
-    serializer = CategorySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['PUT'])
-def update_category(request, category_id):
-    try:
-        category = Category.objects.get(category_id=category_id)
-    except Category.DoesNotExist:
-        return Response({'error': 'Category does not exist'}, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = CategorySerializer(category, request.data)
-
-    if serializer.is_valid:
-        serializer.save()
+class CategoryListView(APIView):
+    def get(self, request):
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-def delete_category(request, category_id):
-    try:
-        Category = Category.objects.get(category_id=category_id)
-    except Category.DoesNotExist:
-        return Response({'error': 'Category Does Not exist'}, status=status.HTTP_404_NOT_FOUND)
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    Category.delete()
-    return Response({'message': 'Category Deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
 
+class CategoryDetailView(APIView):
+    def get_object(self, category_id):
+        try:
+            return Category.objects.get(category_id=category_id)
+        except Category.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, category_id):
+        category = self.get_object(category_id)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, category_id):
+        category = self.get_object(category_id)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, category_id):
+        category = self.get_object(category_id)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 #Notification Model CRUD operations
 
-@api_view(['GET'])
-def get_notifications(request):
-    notifications = Notification.objects.all()
-    serializer = NotificationSerializer(notifications, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+class NotificationListView(APIView):
+    def get(self, request):
+        notification = Notification.objects.all()
+        serializer = NotificationSerializer(notification, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def create_notification(request):
-    serializer = NotificationSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = NotificationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-@api_view(['DELETE'])
-def delete_notification(request, notification_id):
-    try:
-        notification = Notification.objects.get(notification_id=notification_id)
-    except Notification.DoesNotExist:
-        return Response({'error': 'This notification does not exist'}, status=status.HTTP_404_NOT_FOUND)
+class NotificationDetailView(APIView):
+    def get_object(self, notification_id):
+        try:
+            return Notification.objects.get(notification_id=notification_id)
+        except Notification.DoesNotExist:
+            raise Http404
+
+    def get(self, request, notification_id):
+        notification = self.get_object(notification_id)
+        serializer = NotificationSerializer(notification)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    notification.delete()
+    def put(self, request, notification_id):
+        notification = self.get_object(notification_id)
+        serializer = NotificationSerializer(notification, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    return Response({'message': 'Notification deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, notification_id):
+        notification = self.get_object(notification_id)
+        notification.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
