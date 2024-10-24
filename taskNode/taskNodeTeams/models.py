a034e3b1db2f956a358c7049f69d386f3e1fb297
Mouad Laafoura <laafoura.mouad37@gmail.com>
2024-10-25 01:03:32 +0300

    [MOD] Team models

--- taskNode/taskNodeTeams/models.py
+++ taskNode/taskNodeTeams/models.py
@@ -13,14 +13,14 @@ class Team(models.Model):
     team_id = models.AutoField(primary_key=True, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=10000)
-    created_by = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='Team Creator')
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='team_creator')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
 
 
 class TeamMember(models.Model):
     member_id = models.AutoField(primary_key=True, unique=True)
-    team_id = models.ForeignKey(Team.team_id, on_delete=models.CASCADE, related_name='Related Team')
-    user_id = models.ForeignKey(CustomUser.id, on_delete=models.CASCADE, related_name='Related Member')
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team_members')
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='related_member')
     role = models.CharField(max_length=50, choices=ROLES)
     joined_at = models.DateTimeField(auto_now_add=True)
